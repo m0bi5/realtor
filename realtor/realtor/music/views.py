72edd8d8cf0fd6a7a6d376e44335ba9f7863b1ec
m0bi5 <mohitbhasi1998@gmail.com>
2018-09-10 09:55:47 +0530

    ER Diagram, abstract added

---
+++ realtor/realtor/music/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Album
+from django.template import loader
+
+# Create your views here.
+def index(request):
+	all_albums=Album.objects.all()
+	template=loader.get_template('music/index.html')
+	context={'all_albums':all_albums}
+	return render(request,template,context)
+
+def detail(request,album_id):
+	return HttpResponse("<h2>Details for album "+str(album_id)+"</h2>")
